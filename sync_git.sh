93ee804b06103a726033b4d108de09a963b52bba
Sanjay Biswas <sanjaybiswas@Sanjays-MacBook-Air.local>
2025-01-16 00:53:21 +0530

    Your commit message

--- sync_git.sh
+++ sync_git.sh
@@ -2,7 +2,11 @@
 
 set +x
 
-git init
 git add .
-git commit -m "Initial commit"
+git commit -m "Your commit message"
+git push -u origin main
+
+#git init
+#git add .
+#git commit -m "Initial commit"
 
